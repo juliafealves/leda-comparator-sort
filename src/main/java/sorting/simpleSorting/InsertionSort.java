acea258b2895383ecae773afaeda0675dccae77c
Júlia Fernandes <juliafealves@gmail.com>
2018-09-06 15:54:27 -0300

    Primeiro commit

---
+++ src/main/java/sorting/simpleSorting/InsertionSort.java
@@ -0,0 +1,27 @@
+package sorting.simpleSorting;
+
+import sorting.AbstractSorting;
+
+/**
+ * As the insertion sort algorithm iterates over the array, it makes the
+ * assumption that the visited positions are already sorted in ascending order,
+ * which means it only needs to find the right position for the current element
+ * and insert it there.
+ */
+public class InsertionSort<T extends Comparable<T>> extends AbstractSorting<T> {
+
+    @Override
+    public void sort(T[] array, int leftIndex, int rightIndex) {
+        for (int i = leftIndex + 1; i <= rightIndex; i++) {
+            T small = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j].compareTo(small) > 0) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+
+            array[j + 1] = small;
+        }
+    }
+}
